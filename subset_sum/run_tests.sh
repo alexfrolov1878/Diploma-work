9f206b5034db9a01c9529a428229aeaa17fa3bb1
Алексей Фролов <alex@lxf.homenet.org>
2015-04-13 02:29:44 +0500

    uncomment lines in subset_sum/run_tests.sh

--- subset_sum/run_tests.sh
+++ subset_sum/run_tests.sh
@@ -7,39 +7,39 @@ run_tests() {
 	CR_TYPE=$1
 	MEM_TYPE=$2
 	ARGS="${CR_TYPE} ${MEM_TYPE} ${ITERS}"
-	#echo "====================SIZE===================="
-	#${EXE} tests/tests_size/test_100.txt ${ARGS}
-	#${EXE} tests/tests_size/test_200.txt ${ARGS}
-	#${EXE} tests/tests_size/test_300.txt ${ARGS}
-	#${EXE} tests/tests_size/test_400.txt ${ARGS}
-	#${EXE} tests/tests_size/test_500.txt ${ARGS}
-	#${EXE} tests/tests_size/test_600.txt ${ARGS}
-	#${EXE} tests/tests_size/test_700.txt ${ARGS}
-	#${EXE} tests/tests_size/test_800.txt ${ARGS}
-	#${EXE} tests/tests_size/test_900.txt ${ARGS}
-	#${EXE} tests/tests_size/test_1000.txt ${ARGS}
-	#${EXE} tests/tests_size/test_1500.txt ${ARGS}
-	#${EXE} tests/tests_size/test_2000.txt ${ARGS}
-	#${EXE} tests/tests_size/test_2500.txt ${ARGS}
+	echo "====================SIZE===================="
+	${EXE} tests/tests_size/test_100.txt ${ARGS}
+	${EXE} tests/tests_size/test_200.txt ${ARGS}
+	${EXE} tests/tests_size/test_300.txt ${ARGS}
+	${EXE} tests/tests_size/test_400.txt ${ARGS}
+	${EXE} tests/tests_size/test_500.txt ${ARGS}
+	${EXE} tests/tests_size/test_600.txt ${ARGS}
+	${EXE} tests/tests_size/test_700.txt ${ARGS}
+	${EXE} tests/tests_size/test_800.txt ${ARGS}
+	${EXE} tests/tests_size/test_900.txt ${ARGS}
+	${EXE} tests/tests_size/test_1000.txt ${ARGS}
+	${EXE} tests/tests_size/test_1500.txt ${ARGS}
+	${EXE} tests/tests_size/test_2000.txt ${ARGS}
+	${EXE} tests/tests_size/test_2500.txt ${ARGS}
 	echo "==================SIZE_BIG=================="
 	${EXE} tests/tests_size_big/test_3000.txt ${ARGS}
 	${EXE} tests/tests_size_big/test_3500.txt ${ARGS}
 	${EXE} tests/tests_size_big/test_4000.txt ${ARGS}
 	${EXE} tests/tests_size_big/test_4500.txt ${ARGS}
 	${EXE} tests/tests_size_big/test_5000.txt ${ARGS}
-	#echo "====================GOAL===================="
-	#${EXE} tests/tests_goal/test_0_0.txt ${ARGS}
-	#${EXE} tests/tests_goal/test_0_1.txt ${ARGS}
-	#${EXE} tests/tests_goal/test_0_2.txt ${ARGS}
-	#${EXE} tests/tests_goal/test_0_3.txt ${ARGS}
-	#${EXE} tests/tests_goal/test_0_4.txt ${ARGS}
-	#${EXE} tests/tests_goal/test_0_45.txt ${ARGS}
-	#${EXE} tests/tests_goal/test_0_55.txt ${ARGS}
-	#${EXE} tests/tests_goal/test_0_6.txt ${ARGS}
-	#${EXE} tests/tests_goal/test_0_7.txt ${ARGS}
-	#${EXE} tests/tests_goal/test_0_8.txt ${ARGS}
-	#${EXE} tests/tests_goal/test_0_9.txt ${ARGS}
-	#${EXE} tests/tests_goal/test_1_0.txt ${ARGS}
+	echo "====================GOAL===================="
+	${EXE} tests/tests_goal/test_0_0.txt ${ARGS}
+	${EXE} tests/tests_goal/test_0_1.txt ${ARGS}
+	${EXE} tests/tests_goal/test_0_2.txt ${ARGS}
+	${EXE} tests/tests_goal/test_0_3.txt ${ARGS}
+	${EXE} tests/tests_goal/test_0_4.txt ${ARGS}
+	${EXE} tests/tests_goal/test_0_45.txt ${ARGS}
+	${EXE} tests/tests_goal/test_0_55.txt ${ARGS}
+	${EXE} tests/tests_goal/test_0_6.txt ${ARGS}
+	${EXE} tests/tests_goal/test_0_7.txt ${ARGS}
+	${EXE} tests/tests_goal/test_0_8.txt ${ARGS}
+	${EXE} tests/tests_goal/test_0_9.txt ${ARGS}
+	${EXE} tests/tests_goal/test_1_0.txt ${ARGS}
 }
 
 #CR_TYPES=( 1 2 3 4 5 )
