c236abd910e3a15121a326d885decce8e9bca210
Alexey Frolov <alexfrolov1878@gmail.com>
2015-04-17 17:58:52 +0300

    restored original state of run scripts

--- job_scheduling/run_tests.sh
+++ job_scheduling/run_tests.sh
@@ -7,40 +7,41 @@ run_tests() {
 	CR_TYPE=$1
 	MEM_TYPE=$2
 	ARGS="${CR_TYPE} ${MEM_TYPE} ${ITERS}"
-	#echo "====================SIZE===================="
-	#${EXE} tests/tests_size/test_10.txt ${ARGS}
-	#${EXE} tests/tests_size/test_15.txt ${ARGS}
-	#${EXE} tests/tests_size/test_20.txt ${ARGS}
-	#${EXE} tests/tests_size/test_25.txt ${ARGS}
-	#${EXE} tests/tests_size/test_50.txt ${ARGS}
-	#${EXE} tests/tests_size/test_100.txt ${ARGS}
+	echo "====================SIZE===================="
+	${EXE} tests/tests_size/test_10.txt ${ARGS}
+	${EXE} tests/tests_size/test_15.txt ${ARGS}
+	${EXE} tests/tests_size/test_20.txt ${ARGS}
+	${EXE} tests/tests_size/test_25.txt ${ARGS}
+	${EXE} tests/tests_size/test_50.txt ${ARGS}
+	${EXE} tests/tests_size/test_100.txt ${ARGS}
 	echo "==================SIZE_BIG=================="
 	${EXE} tests/tests_size_big/test_250.txt ${ARGS}
 	${EXE} tests/tests_size_big/test_400.txt ${ARGS}
 	${EXE} tests/tests_size_big/test_500.txt ${ARGS}
-	${EXE} tests/tests_size_big/test_600.txt ${ARGS}
+	${EXE} tests/tests_size_big/test_501.txt ${ARGS}
+        ${EXE} tests/tests_size_big/test_600.txt ${ARGS}
 	${EXE} tests/tests_size_big/test_750.txt ${ARGS}
-	#echo "====================CONN===================="
-	#${EXE} tests/tests_conn/test_0_0.txt ${ARGS}
-	#${EXE} tests/tests_conn/test_0_1.txt ${ARGS}
-	#${EXE} tests/tests_conn/test_0_2.txt ${ARGS}
-	#${EXE} tests/tests_conn/test_0_3.txt ${ARGS}
-	#${EXE} tests/tests_conn/test_0_4.txt ${ARGS}
-	#${EXE} tests/tests_conn/test_0_5.txt ${ARGS}
-	#echo "====================DISP===================="
-	#${EXE} tests/tests_disp/test_3.txt ${ARGS}
-	#${EXE} tests/tests_disp/test_5.txt ${ARGS}
-	#${EXE} tests/tests_disp/test_10.txt ${ARGS}
-	#${EXE} tests/tests_disp/test_15.txt ${ARGS}
-	#${EXE} tests/tests_disp/test_20.txt ${ARGS}
-	#${EXE} tests/tests_disp/test_25.txt ${ARGS}
-	#echo "====================RATIO===================="
-	#${EXE} tests/tests_ratio/test_0_02.txt ${ARGS}
-	#${EXE} tests/tests_ratio/test_0_05.txt ${ARGS}
-	#${EXE} tests/tests_ratio/test_0_1.txt ${ARGS}
-	#${EXE} tests/tests_ratio/test_0_2.txt ${ARGS}
-	#${EXE} tests/tests_ratio/test_0_3.txt ${ARGS}
-	#${EXE} tests/tests_ratio/test_0_4.txt ${ARGS}
+	echo "====================CONN===================="
+	${EXE} tests/tests_conn/test_0_0.txt ${ARGS}
+	${EXE} tests/tests_conn/test_0_1.txt ${ARGS}
+	${EXE} tests/tests_conn/test_0_2.txt ${ARGS}
+	${EXE} tests/tests_conn/test_0_3.txt ${ARGS}
+	${EXE} tests/tests_conn/test_0_4.txt ${ARGS}
+	${EXE} tests/tests_conn/test_0_5.txt ${ARGS}
+	echo "====================DISP===================="
+	${EXE} tests/tests_disp/test_3.txt ${ARGS}
+	${EXE} tests/tests_disp/test_5.txt ${ARGS}
+	${EXE} tests/tests_disp/test_10.txt ${ARGS}
+	${EXE} tests/tests_disp/test_15.txt ${ARGS}
+	${EXE} tests/tests_disp/test_20.txt ${ARGS}
+	${EXE} tests/tests_disp/test_25.txt ${ARGS}
+	echo "====================RATIO===================="
+	${EXE} tests/tests_ratio/test_0_02.txt ${ARGS}
+	${EXE} tests/tests_ratio/test_0_05.txt ${ARGS}
+	${EXE} tests/tests_ratio/test_0_1.txt ${ARGS}
+	${EXE} tests/tests_ratio/test_0_2.txt ${ARGS}
+	${EXE} tests/tests_ratio/test_0_3.txt ${ARGS}
+	${EXE} tests/tests_ratio/test_0_4.txt ${ARGS}
 }
 
 #CR_TYPES=( 1 2 3 4 5 )
--- run.sh
+++ run.sh
@@ -1,25 +1,25 @@
 set -x
 
-#cd job_scheduling
-#./run_tests.sh 250
-#mv *.txt results/91/
-#./run_tests.sh 250
-#mv *.txt results/92/
-#./run_tests.sh 250
-#mv *.txt results/93/
-#./run_tests.sh 250
-#mv *.txt results/94/
-#cd ..
+cd job_scheduling
+./run_tests.sh 250
+mv *.txt results/1/
+./run_tests.sh 250
+mv *.txt results/2/
+./run_tests.sh 250
+mv *.txt results/3/
+./run_tests.sh 250
+mv *.txt results/4/
+cd ..
 
 cd subset_sum
 ./run_tests.sh 100
-mv *.txt results/91/
+mv *.txt results/1/
 ./run_tests.sh 100
-mv *.txt results/92/
+mv *.txt results/2/
 ./run_tests.sh 100
-mv *.txt results/93/
+mv *.txt results/3/
 ./run_tests.sh 100
-mv *.txt results/94/
+mv *.txt results/4/
 cd ..
 
 set +x
